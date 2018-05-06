5fbd8b6050fc365d3a06cd5f2437f8e208c2872d
ZERROUG Youcef <zerroug.youcef@gmail.com>
2018-05-06 19:10:29 +0200

    wip

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ apt-get install python3 python3-pip libbz2-dev python3-setuptools python-setupto
 apt-get install tmux
 
 # VIM
+apt-get install vim vim-gtk
 apt-get install aptitude ack-grep exuberant-ctags
