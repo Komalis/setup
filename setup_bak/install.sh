c8ac6194a41e5f0de3f31d9a0d443f2fe3e16a89
ZERROUG Youcef <zerroug.youcef@gmail.com>
2018-05-07 21:14:25 +0200

    wip

---
+++ setup_bak/install.sh
@@ -0,0 +1,10 @@
+# System
+apt-get update && apt-get upgrade
+apt-get install python3 python3-pip libbz2-dev python3-setuptools python-setuptools rsyslog build-essential libssl-dev libffi-dev python-dev htop flake8
+
+# TMUX
+apt-get install tmux
+
+# VIM
+apt-get install vim vim-gtk
+apt-get install aptitude ack-grep exuberant-ctags
