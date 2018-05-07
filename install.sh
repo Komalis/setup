c770c5142cd1fc9b6455d4535ed02dc03d76e1f7
ZERROUG Youcef <zerroug.youcef@gmail.com>
2018-05-07 10:52:25 +0200

    wip

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # System
 apt-get update && apt-get upgrade
-apt-get install python3 python3-pip libbz2-dev python3-setuptools python-setuptools rsyslog build-essential libssl-dev libffi-dev python-dev htop
+apt-get install python3 python3-pip libbz2-dev python3-setuptools python-setuptools rsyslog build-essential libssl-dev libffi-dev python-dev htop flake8
 
 # TMUX
 apt-get install tmux
